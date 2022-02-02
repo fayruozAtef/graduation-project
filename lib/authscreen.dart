e5722ec7512b7a9ba9df1b6218024a8d174b053c
nada443 <77789721+nada443@users.noreply.github.com>
2022-02-02 12:39:24 +0200

    modify

---
+++ lib/authscreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+enum {signup,login}
+class AuthScreen extends StatelessWidget {
+  static const routeName='/auth';
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize= MediaQuery.of(context).size;
+    return Container();
+  }
+}
