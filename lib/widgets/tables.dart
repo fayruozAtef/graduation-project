58eeffa040f4a820e8a5547915057ae8acca360a
rahmamagdi82 <98878812+rahmamagdi82@users.noreply.github.com>
2022-02-02 14:44:20 +0200

    add images

--- lib/widgets/tables.dart
+++ lib/widgets/tables.dart
@@ -9,7 +9,12 @@ void main() {
 
 
 List<String> list = ['assets/table1.jpg',
+  'assets/table1_2',
+];
+
+List<String> list2 = [
   'assets/table2.jpg',
+  'assets/table2_2',
 ];
 
 class tables extends StatelessWidget {
@@ -57,7 +62,7 @@ class tables extends StatelessWidget {
                                     SizedBox(height:20),
                                     SizedBox(
                                         width: 200,
-                                        child: Text("Sets no:2  location:in door",style:TextStyle(color:Colors.black,fontSize: 25))
+                                        child: Text("Sets no:4  location:in door",style:TextStyle(color:Colors.black,fontSize: 25))
                                     ),
                                     SizedBox(height:20),
                                     ElevatedButton(
@@ -70,6 +75,51 @@ class tables extends StatelessWidget {
                                       onPressed: () {},
                                       child: Text('OK',style:TextStyle(fontSize: 30)),
                                     ),
+    SizedBox(height: 20),
+    ElevatedButton(
+    style:ElevatedButton.styleFrom(primary: Colors.white,
+    side:BorderSide(width:3,color:Colors.black),
+    fixedSize:Size(100,100),
+    shape:CircleBorder()
+    ),
+    child: Text('5',style:TextStyle(color:Colors.black,fontSize: 50,fontWeight: FontWeight.bold)),
+    onPressed: () {
+    showModalBottomSheet(
+    context: context,
+    builder: (context) {
+    final theme = Theme.of(context);
+    return Container(
+    height:500,
+    child:Container(
+    child:Column(
+    children:[
+    Container(
+    child: CarouselSlider(
+    options: CarouselOptions(aspectRatio: 1.4,
+    ),
+    items: list2
+        .map((item) => Container(
+    child: Center(child: Image.asset(item)),
+    ))
+        .toList(),
+    )
+    ),
+    SizedBox(height:20),
+    SizedBox(
+    width: 200,
+    child: Text("Sets no:4  location:in door",style:TextStyle(color:Colors.black,fontSize: 25))
+    ),
+    SizedBox(height:20),
+    ElevatedButton(
+    style:ButtonStyle(backgroundColor:MaterialStateProperty.all<Color>(ColorfromRGBO(65,189,180,54)),
+    fixedSize:MaterialStateProperty.all(Size(150,45)),
+    shape:MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+    borderRadius:BorderRadius.circular(18)
+    ))
+    ),
+    onPressed: () {},
+    child: Text('OK',style:TextStyle(fontSize: 30)),
+    ),
                                   ]
                               ),
                             )
