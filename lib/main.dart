08a7583c14912413e7194e9c090a121b52ab2828
fayruozAtef <fayrouz.atef@gmail.com>
2022-02-13 21:42:40 +0200

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:resflutter_app/widgets/home.dart';
+=======
+//import 'package:resflutter_app/auth.dart';
+import 'package:resflutter_app/categories.dart';
+import 'package:resflutter_app/authscreen.dart';
+import 'package:provider/provider.dart';
+import 'authscreen.dart';
+>>>>>>> nadalogin
 
-void main() {
+
+void main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
