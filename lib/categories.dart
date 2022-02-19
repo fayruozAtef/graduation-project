0a2c68f0210c14cfac4b18cc213dfdbdd99d9634
nada443 <77789721+nada443@users.noreply.github.com>
2022-02-19 16:00:34 +0200

    add

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:resflutter_app/widgets/breakfast.dart';
 import 'package:resflutter_app/widgets/confirmdelivery.dart';
